ddfba01300ba249b75101bc0bc8d2177f1230c61
arquitectaluisaramirez-debug <arquitectaluisaramirez@gmail.com>
2026-07-21 18:43:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const video = document.getElementById("video");
 
-video.addEventListener("click", () => {
+const params = new URLSearchParams(window.location.search);
+
+const nombre = params.get("nombre");
+
+document.getElementById("nombre").textContent = nombre || "Nuestro invitado especial";
+
+document.body.addEventListener("click", () => {
 
     video.play();
 
-});
+}, { once:true });
 
 video.addEventListener("ended", () => {
 
-    window.location.href="https://noscasamos-aleyandres.my.canva.site/";
+    window.location.href="https://TU-LINK-DE-CANVA";
 
 });
